29b04fb4049b2c1a7205bc902de81bfe16745508
iaalai <iaalai@iaalai.cn>
2023-04-17 21:24:53 +0800

    4.17 update

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,14 @@
 #include <iostream>
 
-int main(int args,char ** argv) {
+int main(int argc,char ** argv) {
+// 读入文件 和 传入参数
+    for (int i = 0;i < argc;i++) {
+        if (argv[i][0] == '-') {
+
+        } else {
+
+        }
+    }
+
     return 0;
 }
